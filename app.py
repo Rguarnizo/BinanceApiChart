84333142ae37862eb490e1dae5260e643e758bda
rguarnizo <rguarnizo@unal.edu.co>
2022-01-11 20:59:15 -0500

    Historical Candlestick BTC/USDT

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
-from flask import Flask, render_template
+from binance.enums import KLINE_INTERVAL_1DAY
+from flask import Flask, render_template, jsonify
+import config , csv
+from binance.client import Client
 
 app = Flask(__name__)
+client = Client(config.API_KEY,config.API_SECRET);
 
 @app.route("/")
 def index():
-    return render_template("index.html");
 
+    title = 'CoinView'
 
-@app.route("/buy")
+    account = client.get_account();
+    print(account);
+    balances = account['balances'];
+
+    exg_info = client.get_exchange_info();
+    symbols = exg_info['symbols'];
+
+    return render_template("index.html",title=title,balances=balances,symbols=symbols);
+
+@app.route("/buy",methods=['POST'])
 def buy():
+    client.order_market()
     return "buy"
 
-@app.route("/selll")
+@app.route("/sell")
 def sell():
     return "sell"
 
 @app.route("/settings")
 def settings():
     return "settings"
+
+@app.route("/history")
+def history():
+    candlestick = client.get_historical_klines("BTCUSDT",Client.KLINE_INTERVAL_1HOUR,"1 Jan, 2022");
+
+    return  jsonify([{ "time": candle[0],
+        "open": candle[1],
+        "high": candle[2],
+        "low": candle[3],
+        "close": candle[4],} for candle in candlestick]);
--- static/chart.js
+++ static/chart.js
@@ -8,14 +8,30 @@ const chart = LightweightCharts.createChart(document.getElementById("price"), {
 });
 const candlestickSeries = chart.addCandlestickSeries();
 
+fetch("http://127.0.0.1:5000/history", {
+    method: 'GET',
+    headers: new Headers({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+    }),
+}).then((r) =>
+    r.json().then((data) => candlestickSeries.setData(data)),
+);
+
 webSocket.addEventListener("message", function(event) {
     let data = JSON.parse(event.data);
     let info = data.k;
     candlestickSeries.update({
-        time: info.t,
+        time: timeToLocal(info.t),
         open: info.o,
         high: info.h,
         low: info.l,
         close: info.c,
     });
 });
+
+
+function timeToLocal(originalTime) {
+    const d = new Date(originalTime * 1000);
+    return Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds()) / 1000;
+}
